352821b44cf8c00604c6e5bec81881257be12e4a
Gustavo Roos <gustavohroos@gmail.com>
2023-08-24 15:23:44 -0300

    first commit

---
+++ run_benchmarks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+batch_sizes="1 4 8 16 32 48"
+
+for index in {0..12}; do
+    for batch_size in $batch_sizes; do
+        python3 benchmarks.py --index $index --batch-size $batch_size
+    done
+done
