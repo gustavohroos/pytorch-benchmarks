85ec5f56a14bccfdc67f5c2469efaaf14c652c5e
Gustavo Roos <gustavohroos@gmail.com>
2023-08-28 16:43:02 -0300

    add for to run 10 times

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-batch_sizes="1 4 8 16 32 48"
+batch_sizes="1 4 8 16 32"
 
-for index in {0..12}; do
-    for batch_size in $batch_sizes; do
-        python3 orchestrator.py --index $index --batch-size $batch_size
+for j in {0..10}; do
+    for index in {0..12}; do
+        for batch_size in $batch_sizes; do
+            python3 orchestrator.py --index $index --batch-size $batch_size
+        done
     done
 done
